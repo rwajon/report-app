dd8d42ffcabe64e924fb4bb91279a278705775a7
Jonathan Rwabahizi <jonathanr.wabahizi@Jonathan-Rwabahizi-MacBook-Pro.local>
2019-04-15 11:58:44 +0200

    [Chore #165344888] Set up the environment and the codebase

---
+++ src/models/aaa.js
@@ -0,0 +1,10 @@
+import User from './index';
+
+User.create({
+  fullName: 'Rwabahizi Jonathan',
+  userName: 'rwajon',
+  phone: '+250781146646',
+  email: 'jonathanrwabahizi@gmail.com',
+  password: '12345',
+  isAdmin: true,
+});
