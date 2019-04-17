b0c44c78ad0d3220318a8d725daacc198961527f
Jonathan Rwabahizi <jonathanr.wabahizi@Jonathan-Rwabahizi-MacBook-Pro.local>
2019-04-17 15:26:19 +0200

    Chore #165415322] Allow cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import usersRouter from './routes/users';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({
