08bc50f18a9a75d8edd021171b55f4f6650716db
Jonathan Rwabahizi <jonathanr.wabahizi@Jonathan-Rwabahizi-MacBook-Pro.local>
2019-04-18 14:04:47 +0200

    [Chore #165415322] Allow cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import usersRouter from "./routes/users";
 import reportsRouter from "./routes/reports";
 import organizationRouter from "./routes/organizations";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(
