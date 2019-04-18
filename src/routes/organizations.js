49f6baf8d2bdeec8fb40e004c01076762f32baa3
SENGAYIRE Prince <psengayire@gmail.com>
2019-04-18 08:02:00 +0200

    [feature 165332267] delete a specific organization

--- src/routes/organizations.js
+++ src/routes/organizations.js
@@ -4,5 +4,6 @@ import organisationController from "../controllers/organizationController";
 const router = Router();
 
 router.post("/newOrganization", organisationController.create);
+router.delete("/organizations/:id", organisationController.deleteOrganization);
 
 export default router;
